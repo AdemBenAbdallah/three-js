aca70aabc85b2121b6f2f83822bf44e49a713853
AdmeBenAbdallah <adembenabdallah.contact@gmail.com>
2024-08-28 20:49:08 +0100

    animate object

--- part_1/lesson01/animation.js
+++ part_1/lesson01/animation.js
@@ -1,8 +1,20 @@
-import gsap from "gsap";
 import * as THREE from "three";
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
+
+// Crusor
+const cursor = { x: 0, y: 0 };
+window.addEventListener("mousemove", (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = -(e.clientY / sizes.height - 0.5);
+});
 
 // Scene
 const scene = new THREE.Scene();
@@ -13,14 +25,13 @@ const material = ({ color }) => new THREE.MeshBasicMaterial({ color: color });
 const mesh = new THREE.Mesh(geometry, material({ color: 0xff0000 }));
 scene.add(mesh);
 
-// Sizes
-const sizes = {
-  width: 800,
-  height: 600,
-};
-
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 camera.position.z = 3;
 scene.add(camera);
 
@@ -32,25 +43,11 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 // Animations
-gsap.to(mesh.position, {
-  duration: 1,
-  delay: 1,
-  x: 2,
-});
-
-gsap.to(mesh.position, {
-  duration: 2,
-  delay: 5,
-  x: 0,
-});
-
-// const clock = new THREE.Clock();
 const tick = () => {
-  // const elapsedTime = clock.getElapsedTime();
-  // camera.position.x = Math.sin(elapsedTime);
-  // camera.position.y = Math.cos(elapsedTime);
-
-  // camera.lookAt(mesh.position);
+  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  camera.position.y = cursor.y * 5;
+  camera.lookAt(mesh.position);
 
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
