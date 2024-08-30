365eb88529574d3ccdbf4de7aa53a068100d4318
AdmeBenAbdallah <adembenabdallah.contact@gmail.com>
2024-08-30 15:41:13 +0100

    buffer Attributes

--- part_1/lesson01/buffer.js
+++ part_1/lesson01/buffer.js
@@ -31,50 +31,47 @@ window.addEventListener("dblclick", () => {
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
-// Crusor
-const cursor = { x: 0, y: 0 };
-window.addEventListener("mousemove", (e) => {
-  cursor.x = e.clientX / sizes.width - 0.5;
-  cursor.y = -(e.clientY / sizes.height - 0.5);
-});
-
 // Scene
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const geometry = new THREE.BufferGeometry();
+
+const count = 500;
+const positionsArray = new Float32Array(count * 3 * 3);
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = Math.random() - 0.5;
+}
+
+const positionAttribute = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute("position", positionAttribute);
+
 const material = ({ color }) =>
   new THREE.MeshBasicMaterial({ color: color, wireframe: true });
 const mesh = new THREE.Mesh(geometry, material({ color: 0xff0000 }));
 scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100,
-);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(1, 1, 5);
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
+// Camera Controls
+camera.lookAt(mesh.position);
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.render(scene, camera);
 
 // Animations
 const tick = () => {
-  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  // camera.position.y = cursor.y * 5;
   camera.lookAt(mesh.position);
 
   renderer.render(scene, camera);
